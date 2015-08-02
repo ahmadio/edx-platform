d046a645f4032951ac7249c3a932c72629b31666
Ahmad AbdArrahman <ahmadio@users.noreply.github.com>
2015-08-02 17:11:24 +0300

    pages admin theme poilerblate working as manager index

--- lms/djangoapps/manager/views.py
+++ lms/djangoapps/manager/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from edxmako.shortcuts import render_to_response
 
 
 def manager(request):
-	return HttpResponse('"<html><body>helelleleo.</body></html>"')
+
+	return render_to_response('manager/pages-admin-theme-main.html', {})
